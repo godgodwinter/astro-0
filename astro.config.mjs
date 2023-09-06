e5f8974c422fb1bd82d30ea2a7c795b75fe8174f
godgodwinter <kakadz.pro@gmail.com>
2023-09-06 16:20:58 +0700

    testing deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/static";
+// import vercel from "@astrojs/vercel/static";
 // import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
@@ -8,10 +8,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercel({
-    imagesConfig: {
-      sizes: [320, 640, 1280],
-    },
-  }),
+  // output: "server",
+  // adapter: vercel({
+  //   imagesConfig: {
+  //     sizes: [320, 640, 1280],
+  //   },
+  // }),
 });
